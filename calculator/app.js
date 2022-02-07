e9a3b9e0acc5181c2b84213abf71483022f3f5d8
avi123ash <93993494+avi123ash@users.noreply.github.com>
2022-02-07 22:42:45 +0545

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,15 @@
+const display1El =document.querySelector('.display-1');
+const display2El =document.querySelector('.display-2');
+const tempresultEl =document.querySelector('.temp-result');
+const numberEl = document.querySelector('.number');
+const operationEl = document.querySelector('.operation');
+const clearEl = document.querySelector('.all-clear');
+const clearLastEl = document.querySelector('.last-entity-clear');
+
+let dis1Num = '';
+let dis2Num = '';
+let result = null;
+let lastOperation = '';
+let haveDot = false;
+ 
+ 
